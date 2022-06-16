0b34d8aafe6b42828b453b30cd279c3138c7a3e6
Febrian Amir <febrianamir5@gmail.com>
2022-06-16 14:59:15 +0000

    initialize config

---
+++ config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	Port               string
+	ServerAddress      string
+	SecretKey          string
+	DBSource           string
+	GoogleClientID     string
+	GoogleClientSecret string
+	GoogleRedirectUrl  string
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Couldn't loading .env file")
+	}
+
+	Port = os.Getenv("PORT")
+	ServerAddress = os.Getenv("SERVER_ADDRESS")
+	SecretKey = os.Getenv("SECRET_KEY")
+	DBSource = os.Getenv("DB_SOURCE")
+	GoogleClientID = os.Getenv("GOOGLE_CLIENT_ID")
+	GoogleClientSecret = os.Getenv("GOOGLE_CLIENT_SECRET")
+	GoogleRedirectUrl = os.Getenv("GOOGLE_REDIRECT_URL")
+}
