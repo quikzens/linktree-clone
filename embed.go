30b5d2c69e94625b7cc66f59d83cc5ebf9fb6b7b
Febrian Amir <febrianamir5@gmail.com>
2022-06-16 15:01:21 +0000

    embed frontend ui to fs

---
+++ embed.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"embed"
+	"net/http"
+)
+
+//go:embed _ui/build
+var UI embed.FS
+
+type embedFileSystem struct {
+	http.FileSystem
+	indexes bool
+}
+
+func (e embedFileSystem) Exists(prefix string, path string) bool {
+	f, err := e.Open(path)
+	if err != nil {
+		return false
+	}
+
+	// check if indexing is allowed
+	s, _ := f.Stat()
+	if s.IsDir() && !e.indexes {
+		return false
+	}
+
+	return true
+}
