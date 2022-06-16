0f72b8afe6427524a5af3eafc2f952116ab60e0a
Febrian Amir <febrianamir5@gmail.com>
2022-06-16 17:57:09 +0000

    ui: admin page

---
+++ _ui/src/pages/Admin/Admin.js
@@ -0,0 +1,12 @@
+export default function Admin() {
+  return (
+    <div className="columns">
+      <div className="column">
+        <h1>Admin Page</h1>
+      </div>
+      <div className="column is-one-third">
+        <h1>Preview</h1>
+      </div>
+    </div>
+  )
+}
