11113fb4d42617159f18ffc54c34ec43df92f96f
Febrian Amir <febrianamir5@gmail.com>
2022-06-16 15:06:42 +0000

    user: callback oauth handler

--- domain/user/user_cont.go
+++ domain/user/user_cont.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/objx"
 )
 
 func Login(c *gin.Context) {
@@ -27,4 +28,39 @@ func Login(c *gin.Context) {
 
 	c.Status(http.StatusTemporaryRedirect)
 	c.Header("Location", loginUrl)
+}
+
+func Callback(c *gin.Context) {
+	providerParam := c.Param("provider")
+
+	provider, err := gomniauth.Provider(providerParam)
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Error when trying to get provider %s: %s", provider, err))
+		c.Abort()
+		return
+	}
+
+	creds, err := provider.CompleteAuth(objx.MustFromURLQuery(c.Request.URL.RawQuery))
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error when trying to complete auth for %s: %s", provider, err))
+		c.Abort()
+		return
+	}
+
+	user, err := provider.GetUser(creds)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error when trying to get user from %s: %s", provider, err))
+		c.Abort()
+		return
+	}
+
+	authCookieValue := objx.New(map[string]interface{}{
+		"name":       user.Name(),
+		"email":      user.Email(),
+		"avatar_url": user.AvatarURL(),
+	}).MustBase64()
+
+	c.SetCookie("auth", authCookieValue, 60*15, "/", "", false, true)
+	c.Status(http.StatusTemporaryRedirect)
+	c.Header("Location", "/")
 }
