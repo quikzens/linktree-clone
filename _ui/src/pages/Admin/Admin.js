aac02e9aa0c98417274a98c484714accc8b19e4c
Febrian Amir <febrianamir5@gmail.com>
2022-06-20 17:13:16 +0000

    pages: admin

--- _ui/src/pages/Admin/Admin.js
+++ _ui/src/pages/Admin/Admin.js
@@ -1,11 +1,13 @@
+import LinkEditor from './LinkEditor/LinkEditor'
+
 export default function Admin() {
   return (
     <div className="columns">
-      <div className="column">
-        <h1>Admin Page</h1>
+      <div className="column p-6">
+        <LinkEditor />
       </div>
-      <div className="column is-one-third">
-        <h1>Preview</h1>
+      <div className="column is-one-third p-6">
+        <div className="link-preview"></div>
       </div>
     </div>
   )
