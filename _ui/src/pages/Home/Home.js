dc6ae36cf8ed90edc36a26ee42faffda75310d61
Febrian Amir <febrianamir5@gmail.com>
2022-06-22 14:12:24 +0000

    improve homepage ui

--- _ui/src/pages/Home/Home.js
+++ _ui/src/pages/Home/Home.js
@@ -1,43 +1,34 @@
 import { Link } from 'react-router-dom'
 import { useUser } from '../../contexts/UserContext'
+import './Home.css'
 
 export default function Home() {
   const { isLoggedIn } = useUser()
 
   return (
-    <div>
-      <nav
-        className="navbar is-flex is-justify-content-space-between"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="navbar-brand">
-          <a className="navbar-item is-size-4 has-text-weight-bold" href="/">
-            Linktree Clone
-          </a>
-        </div>
+    <div className="page-home is-flex is-justify-content-center is-align-items-center">
+      <div className="home-container w-60 is-flex is-flex-direction-column is-align-items-center">
+        <h1 className="title">
+          Linktree <br /> Clone
+        </h1>
 
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              {isLoggedIn ? (
-                <>
-                  <Link to="/admin" className="button is-primary">
-                    <strong>Admin</strong>
-                  </Link>
-                  <a className="button" href="/auth/logout">
-                    <strong>Log Out</strong>
-                  </a>
-                </>
-              ) : (
-                <a className="button is-primary" href="/auth/login/google">
-                  <strong>Log In</strong>
-                </a>
-              )}
-            </div>
-          </div>
+        <div className="buttons">
+          {isLoggedIn ? (
+            <>
+              <Link to="/admin" className="button is-primary">
+                <strong>Admin</strong>
+              </Link>
+              <a className="button" href="/auth/logout">
+                <strong>Log Out</strong>
+              </a>
+            </>
+          ) : (
+            <a className="button is-primary" href="/auth/login/google">
+              <strong>Log In</strong>
+            </a>
+          )}
         </div>
-      </nav>
+      </div>
     </div>
   )
 }
